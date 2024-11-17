e812e51f5b742ce273bfb5e35965b508bb12e3ff
Jonthan Machado <jonathan.machado@justeattakeaway.com>
2024-11-17 16:33:12 +0100

    Added first test and baseUrl

---
+++ cypress/e2e/features/add-item.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('add item', () => {
+    beforeEach(() => {
+        cy.visit('/')
+
+        
+    })
+
+    it('adds an item to basket', () => { 
+        cy.get(`[aria-label="Add item"]`).first().click()
+        cy.get('.p-4').should('include.text', 'Bruschetta')
+
+    })
+
+
+})
