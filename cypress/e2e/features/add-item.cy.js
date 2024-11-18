e2e8dde6a216951053d5303b741ed6fea686df25
Jonthan Machado <jonathan.machado@justeattakeaway.com>
2024-11-18 16:58:06 +0100

    added test for restricted item to basket

--- cypress/e2e/features/add-item.cy.js
+++ cypress/e2e/features/add-item.cy.js
@@ -7,10 +7,28 @@ describe('add item', () => {
         
     })
 
-    it('adds an item to basket', () => { 
-        cy.get(`[aria-label="Add item"]`).first().click()
-        cy.get('.p-4').should('include.text', 'Bruschetta')
+    it('adds a simple item to basket', () => { 
+        cy.get(`[aria-label="Add item"]`)
+            .first()
+            .click()
+        cy.get('.p-4')
+            .should('include.text', 'Bruschetta')
+    })
 
+    it('adds restricted item to basket', () => { 
+        cy.get('[placeholder="Search items of this restaurant"]')
+            .click()
+            .type('Beer');
+        cy.get(`[data-qa="item-card"]`)
+            .first()
+            .click()
+        cy.get(`[data-qa="item-selector-dialog-content"]`)
+            .should('exist')
+        
+        cy.get(`[data-qa="item-selector-add-button"]`).click()
+        
+        cy.get('.p-4')
+            .should('include.text', 'Beer')
     })
 
 })
